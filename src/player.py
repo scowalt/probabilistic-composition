8f5e1a713cb7e7101e356b8335f2d46d32932938
Scott Walters <me@scowalt.com>
2014-06-09 15:29:44 -0500

    Basic progression playing

--- src/player.py
+++ src/player.py
@@ -1,16 +1,29 @@
 import progression_gen as gen 
 from simpleOSC import initOSCClient, sendOSCMsg
-import time
+from time import sleep
+from mingus.core import *
 
 ip = "127.0.0.1"
 port = 9002
 initOSCClient(ip, port)
+chord_octave = 5
 
-def play(chord):
-	pass
+def play_chord(chord):
+	sendOSCMsg("/note", [1, note_to_midi(chord[0])])
+	sendOSCMsg("/note", [2, note_to_midi(chord[1])])
+	sendOSCMsg("/note", [3, note_to_midi(chord[2])])
+
+def note_to_midi(n):
+	return notes.note_to_int(n) + chord_octave * 12
 
 if __name__ == "__main__":
-	sendOSCMsg("/note", [1, 66])
-	sendOSCMsg("/note", [2, 70])
-	sendOSCMsg("/note", [3, 73])
-	sendOSCMsg("/note", [4, 90])
+	# sendOSCMsg("/note", [1, 42])
+	# sendOSCMsg("/note", [2, 58])
+	# sendOSCMsg("/note", [3, 61])
+	# sendOSCMsg("/note", [4, 78])
+	g = gen.generate()
+	print g
+	p = progressions.to_chords(g, "C")
+	for c in p:
+		play_chord(c)
+		sleep(1)
